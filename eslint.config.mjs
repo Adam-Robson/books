854a0fe10bba61a6b56b81a591c893298be106ba
Adam Robson <adamrayrobson@gmail.com>
2024-07-29 12:19:43 -0700

    update eslint config to flat config, and add secretes to repo and yaml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,64 @@
-import globals from "globals";
-import js from "@eslint/js";
+import globals from "globals"
+import path from "path"
+import js from "@eslint/js"
 
 export default [
-  js.configs.recommended,
-  {
-    name: 'configuration',
-    files: ['**/*.js'],
+    js.configs.recommended,
+    {
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-        ...globals.node,
-        QUnit: false,
-        supabase: false
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          impliedStrict: true
+        ecmaVersion: 2021,
+        sourceType: "module",
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+            ...globals.node,
+            ...globals.qunit,
+            ...globals.supabase
+            },
         }
-      }
-    }
-  }
-];
+    },
+    {
+        rules: {
+            "no-console": "off",
+            "no-multi-spaces": "error",
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+            "no-var": "error",
+            "prefer-const": "error",
+            "new-cap": ["error", {
+                capIsNew: false
+            }],
+            "no-redeclare": "error",
+            "no-use-before-define": "error",
+            "space-in-parens": ["error"],
+            "space-infix-ops": "error",
+            "object-curly-spacing": ["error", "always"],
+            "comma-spacing": "error",
+            "space-before-function-paren": ["error", {
+                anonymous: "never",
+                named: "never",
+                asyncArrow: "always",
+            }],
+            "keyword-spacing": ["error", {
+                before: true,
+                after: true,
+            }],
+            "array-bracket-spacing": "error",
+            eqeqeq: ["error", "always"],
+            indent: ["error", 2, {
+                SwitchCase: 1,
+                ignoreComments: true,
+            }],
+            quotes: ["error", "single", {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            }],
+
+            semi: ["error", "always"],
+                
+            }
+        },
+        {
+            ignores: ["eslint.config.mjs"]
+        }
+    ];
